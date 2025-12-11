9e20ef4b575a913df623e9384aa84889c47bb087
Srijan Gupta <Srijan.Gupta@iiitb.ac.in>
2025-12-11 14:16:25 +0000

    Sync LeetCode submission - Fibonacci Number (cpp)

---
+++ problems/fibonacci_number/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int fib(int n) {
+        if (n<2) {
+            return n;
+        }
+        long long prev1 = 1;
+        long long prev2 = 0;
+        long long curr = 1;
+        for (int i = 2; i<=n ; i++) {
+            curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+
+        }
+        return curr;
+    }
+};
