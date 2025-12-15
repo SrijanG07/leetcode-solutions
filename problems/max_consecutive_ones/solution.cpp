2b58f6a2e845d88c7da227284f468d2ef0e114ed
Srijan Gupta <Srijan.Gupta@iiitb.ac.in>
2025-12-15 02:33:16 +0000

    Sync LeetCode submission - Max Consecutive Ones (cpp)

---
+++ problems/max_consecutive_ones/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int wind = 0;
+        int ans = 0;
+        for (int i = 0; i<nums.size() ; i++) {
+            wind++;
+            if (nums[i] == 0) {
+                ans = max(ans,wind-1);
+                wind = 0;
+            }
+        }
+        return max(ans,wind);
+    }
+};
